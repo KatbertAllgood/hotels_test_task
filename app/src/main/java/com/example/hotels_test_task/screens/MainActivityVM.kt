3094b70c885323e3325abfd9535787078e5f720a
KatbertAllgood <srazmi@mail.ru>
2023-11-24 15:02:32 +0300

    created view model for main activity

---
+++ app/src/main/java/com/example/hotels_test_task/screens/MainActivityVM.kt
@@ -0,0 +1,10 @@
+package com.example.hotels_test_task.screens
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainActivityVM @Inject constructor() : ViewModel() {
+
+}
