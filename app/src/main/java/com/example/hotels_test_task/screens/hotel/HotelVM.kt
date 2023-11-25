405e3ec080c8965672c0cf14bf45f87bf5b9e827
KatbertAllgood <srazmi@mail.ru>
2023-11-25 22:39:49 +0300

    added logic to viewmodel for hotel fragment

--- app/src/main/java/com/example/hotels_test_task/screens/hotel/HotelVM.kt
+++ app/src/main/java/com/example/hotels_test_task/screens/hotel/HotelVM.kt
@@ -1,8 +1,19 @@
 package com.example.hotels_test_task.screens.hotel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.domain.usecase.hotel.GetHotelInfoUseCase
+import com.example.hotels_test_task.mappers.toPresentation
+import com.example.hotels_test_task.models.hotel.HotelInfoPresentation
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,4 +21,30 @@ class HotelVM @Inject constructor(
     private val getHotelInfoUseCase: GetHotelInfoUseCase
 ) : ViewModel() {
 
+    private val TAG = HotelVM::class.simpleName
+
+    private var hotelInfoLiveData = MutableLiveData<HotelInfoPresentation>()
+    val getHotelInfo: LiveData<HotelInfoPresentation>
+        get() = hotelInfoLiveData
+
+    private var hotelName : String = ""
+    val getHotelName : String
+        get() = hotelName
+
+    init {
+        getHotelInfo()
+    }
+
+    private fun getHotelInfo() {
+        viewModelScope.launch {
+            getHotelInfoUseCase().flowOn(Dispatchers.IO).catch {
+                Log.d(TAG, it.message.toString())
+            }.map {
+                it.toPresentation()
+            }.collect {
+                hotelInfoLiveData.postValue(it)
+                hotelName = it.name
+            }
+        }
+    }
 }
