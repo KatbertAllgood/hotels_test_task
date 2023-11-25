2524e1650e68cefdfe57054f3717deb2dc99dceb
KatbertAllgood <srazmi@mail.ru>
2023-11-25 22:36:19 +0300

    added SerializedName annotation

--- data/src/main/java/com/example/data/models/rooms/RoomData.kt
+++ data/src/main/java/com/example/data/models/rooms/RoomData.kt
@@ -1,12 +1,19 @@
 package com.example.data.models.rooms
 
 import com.example.domain.models.rooms.RoomDomain
+import com.google.gson.annotations.SerializedName
 
 data class RoomData(
+    @SerializedName("id")
     override val id: Int = 0,
+    @SerializedName("name")
     override val name: String = "",
+    @SerializedName("price")
     override val price: Int = 0,
+    @SerializedName("price_per")
     override val price_per: String = "",
+    @SerializedName("peculiarities")
     override val peculiarities: List<String> = listOf(),
+    @SerializedName("image_urls")
     override val image_urls: List<String> = listOf()
 ) : RoomDomain
